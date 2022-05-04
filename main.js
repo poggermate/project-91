0d2a289e47ba38cc96bc7913882860fc36a789f7
poggermate <77425501+poggermate@users.noreply.github.com>
2022-05-04 16:03:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,30 @@ function adduser() {
 
     window.location.replace("game_page.html");
 }
+question_turn = "player1";
+answer_turn = "player2";
+function check(){
+    get_answer = document.getElementById("input_check_box").value;
+    if(get_answer == actual_answer)
+    {
+        if(answer_turn == "player1")
+        {
+            update_player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = update_player1score;
+        }   
+        else
+        {   update_player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = update_player2score;
+        }
+}
+        
+if (question_turn= "player1")
+{
+     question_turn = "player2
+     docunent.getElementById("player_question").innerHTML = "Question Turn - "+ player2_name ;
+}
+else
+{
+     question_turn = "player1"
+     docunent.getElenentById("player_question").innertHTML = "Question Turn - "+ player1_nane ;
+}
